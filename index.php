dface06d53cb5c5c4db4bfd140018106e9e0c8d1
rsilber12 <105246385+rsilber12@users.noreply.github.com>
2023-07-09 14:44:49 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookNook</title>
+
+    <link rel="stylesheet" type="text/css" href="assest/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assest/css/responsive.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" />
+
+</head>
+<body>
+    <div class="overflow-hidden">
+        <div class="header-top">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <div class="container">
+                    <a class="navbar-brand" href="index.php">
+                        <img src="assest/images/logo.svg" alt="" class="img-fluid logo1">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-lg-auto">
+                            <div class="d-block d-lg-none mb-5 text-center">
+                                <button class="navbar-toggler fa-2x" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+                                    <span>×</span>
+                                </button>
+                            </div>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.php">About</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="works.php">Works</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.php">Contact</a>
+                            </li>
+
+                            <!-- <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Services
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">App development</a></li>
+                                    <li><a class="dropdown-item" href="#">web development</a></li>
+                                    <li><a class="dropdown-item" href="#">UX & UI design</a></li>
+                                    <li><a class="dropdown-item" href="#">Digital business development</a></li>
+                                </ul>
+                            </li> -->
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class="banner-section position-relative overflow-hidden">
+            <img src="assest/images/booknook-pattern.png" alt="" class="img-fluid w-100 booknook-pattern">
+            <div class="overlay d-flex flex-column justify-content-center padding">
+                <div class="container">
+                    <div class="row text-center">
+                        <div class="space-top"></div>
+                        <div class="col-xl-8 col-lg-10 col-12 mx-auto text-white">
+                            <h2 class="display-6 mb-4 pb-2" data-aos="fade-up" data-aos-delay="100">Welcome to BookNook <br> <span class="text-main">Custom Study Room Furniture Solutions</span></h2>
+                        </div>
+                        <div class="col-xl-6 col-lg-8 col-12 mx-auto text-white">
+                            <p class="mb-0 text-white-80" data-aos="fade-up" data-aos-delay="200">Discover a world of possibilities for your study room with BookNook. <br> We specialize in creating custom study rooms that are tailored to your unique needs and style. 
+                                <span class="font-chirp_medium text-white">Explore our range of services below to transform your study space into a haven of productivity and inspiration.</span>
+                            </p>
+
+                            <div class="arrow mt-5" data-aos="fade-up" data-aos-delay="300"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="py-2 py-md-5" style="background: #c7b58b;">
+            <div class="py-4"></div>
+        </div>
+
+        <div class="section-01 position-relative overflow-hidden">
+            <img src="assest/images/booknook-pattern.png" alt="" class="img-fluid w-100 booknook-pattern">
+            <div class="padding">
+                <div class="container">
+                    <div class="row g-3 position-relative">
+                        <div class="col-lg-6 col-12">
+                            <div class="box1 mb-3" data-aos="fade-up" data-aos-delay="100">
+                                <div class="py-sm-5 py-4">
+                                    <h2 class="display-6 mb-0 text-white"><span class="text-1c">Custom Study <br> Rooms:</span> <br> Unleash Your <br> Potential</h2>
+                                </div>
+                            </div>
+                            <div class="box1" data-aos="fade-up" data-aos-delay="200" style="background: #1c1c1c;">
+                                <p class="text-white-80 mb-0 mb-lg-5">Imagine stepping into a study room that perfectly reflects your personality and supports your academic or professional endeavors. At BookNook, we specialize in creating custom study rooms that combine functionality with stunning design. Our experienced team of designers will work closely with you to understand your vision and bring it to life. Whether you need built-in bookcases, desks, tables, chairs, or lighting fixtures,
+                                <br> <span class="font-chirp_medium text-white">we have the expertise to transform your study room into a space that inspires productivity and sparks joy.</span></p>
+                                        
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-12 position-relative">
+                            <img src="assest/images/builtin-bookcases-with-integrated-lighting-ai-generated.jpg" alt="" class="w-100 h-100 img1" style="object-fit: cover;">
+                            <div class="">
+                                <a href="contact.php" class="btn btn1 btn-arrow text-nowrap" data-aos="zoom-in" data-aos-delay="200">Contact Us Today</a>
+                            </div>
+                        </div>
+                        <div class="arrow2"></div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="padding pt-0">
+                <div class="container">
+                    <div class="row g-3 position-relative">
+                        <div class="col-lg-6 col-12 order-lg-1">
+                            <div class="box1 box-h d-flex flex-column justify-content-center bg-white" data-aos="fade-up" data-aos-delay="100">
+                                <div class="py-sm-5 py-4">
+                                    <h2 class="display-6 mb-0 text-main"><span class="text-1c">Quality <br> Craftsmanship:</span> <br> Made to Last</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-12 order-lg-2">
+                            <div class="box1 box-h d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+                                <p class="mb-0 pb-lg-5">At BookNook, we take pride in our dedication to excellent craftsmanship. Our skilled artisans carefully build each piece of furniture with great attention to detail, using high-quality materials. 
+                                    <br> <span class="font-chirp_medium text-black">We believe in creating furniture that not only meets your immediate needs but also withstands the test of time, providing you with a durable and reliable study room solution.</span></p>
+                                    
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-12">
+                            <div class="position-relative wood-bg">
+                                <img src="assest/images/man-using-circular-saw-cut-piece-wood-2.jpg" alt="" class="w-100 img2" style="object-fit: cover;">
+                                <img src="assest/images/booknook-icon.svg" alt="" class="img-fluid wood-icon">
+                            </div>
+                            <div class="">
+                                <a href="contact.php" class="btn btn1 btn-white btn-arrow mt-3 text-nowrap" data-aos="zoom-in" data-aos-delay="200">Contact Us Today</a>
+                            </div> 
+                        </div>
+                        <div class="arrow2 right"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-01" style="background: #c7b58b;">
+            <div class="padding">
+                <div class="container">
+                    <div class="row g-3 position-relative">
+                        <div class="col-lg-6 col-12">
+                            <div class="box1 mb-3 bg-white" data-aos="fade-up" data-aos-delay="100">
+                                <div class="py-sm-5 py-4">
+                                    <h2 class="display-6 mb-0 text-main"><span class="text-1c">Tables, Desks, <br> and Chairs:</span> <br> Ergonomic and <br> Stylish</h2>
+                                </div>
+                            </div>
+                            <div class="box1" data-aos="fade-up" data-aos-delay="200" style="background: #1c1c1c;">
+                                <p class="text-white-80 mb-0 mb-lg-5">We understand the importance of comfortable and functional furniture in a study room. Our expert craftsmen combine ergonomics with exquisite design to deliver tables, desks, and chairs that enhance your posture, support long hours of work, and elevate the overall aesthetic of your space.
+                                <br> <span class="font-chirp_medium text-white">From sleek modern designs to classic and timeless pieces, our furniture collection is meticulously crafted using high-quality materials, ensuring durability and longevity.</span></p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <img src="assest/images/desk-with-glass-wall-bookcase-with-view-city-tokyo-background.jpg" alt="" class="w-100 h-100 img1" style="object-fit: cover;">
+                            <div class="">
+                                <a href="contact.php" class="btn btn1 btn-white btn-arrow text-nowrap" data-aos="zoom-in" data-aos-delay="200">Contact Us Today</a>
+                            </div>
+                        </div>
+                        <div class="arrow2"></div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="padding pt-0">
+                <div class="container">
+                    <div class="row g-3 position-relative">
+                        <div class="col-lg-6 col-12 order-lg-1">
+                            <div class="box1 box-h d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100" style="background: #1c1c1c;">
+                                <div class="py-sm-5 py-4">
+                                    <h2 class="display-6 mb-0 text-main"><span class="text-white">Illuminate Your <br> Study Space:</span> <br> Lighting Solutions</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-12 order-lg-2">
+                            <div class="box1 bg-white box-h d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+                                <p class="mb-0 pb-lg-5">Proper lighting is essential for creating a conducive study environment. BookNook offers a range of lighting solutions that strike the perfect balance between functionality and ambiance. 
+                                    <br> <span class="font-chirp_medium text-black">Whether you need task lighting for focused work, ambient lighting for a cozy atmosphere, or adjustable options to cater to different activities, our lighting fixtures will transform your study room into a well-lit oasis.</span></p>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-12">
+                            <img src="assest/images/builtin-bookcases-with-integrated-lighting-ai-generated-2.jpg" alt="" class="w-100 img2" style="object-fit: cover; object-position: bottom;">
+                            <div class="">
+                                <a href="contact.php" class="btn btn1 btn-black btn-arrow mt-3 text-nowrap" data-aos="zoom-in" data-aos-delay="200">Contact Us Today</a>
+                            </div> 
+                        </div>
+                        <div class="arrow2 right black"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer class="footer-section">
+            <div class="container">
+                <hr class="border-white mb-5 opacity-100">
+                <div class="row mb-5 pb-md-3">
+                    <div class="col-md-4 col-sm-9 col-8">
+                        <a href="index.php">
+                            <img src="assest/images/logo.svg" alt="" class="img-fluid logo-ftr">
+                        </a>
+                    </div>
+                    <div class="col-md-8 col-sm-3 col-4">
+                        <nav class="nav nav1 justify-content-lg-end">
+                            <a class="nav-link active" href="index.php">Home</a>
+                            <a class="nav-link" href="about.php">About</a>
+                            <a class="nav-link" href="works.php">Works</a>
+                            <a class="nav-link" href="contact.php">Contact</a>
+                        </nav>
+                    </div>
+                </div>
+                <p class="fs-14 text-white-60 mb-0">© 2023 BookNook Design. All rights reserved  |  Designed by Avrumy</p>
+            </div>
+        </footer>
+    </div>
+
+    <script type="text/javascript" src="assest/js/SmoothScroll.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+    <script type="text/javascript" src="assest/js/script.js"></script>
+</body>
+</html>
